d7c1c909d267cbb7123c8d2691de7881d479fbab
Eric Farris <efarris1993@gmail.com>
2023-06-26 07:34:57 -0400

    needs work

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+MediaDeviceInfo.remove()
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute= ("id" , "Victory");
+
+newHeader.textcontent = "Eric Farris is the champion";
+
+document.body.appendChild(h1);
